92ea46fa46e36191a7c9a70d840f14cf7bafe7d7
Joe Kanter <kanterjoe@gmail.com>
2018-06-30 15:46:09 -0500

    Created an observable from fetch Promise.
    Can now fire <observable>.next() to get new observables from HTTP responses.

--- ajaxStream.js
+++ ajaxStream.js
@@ -1,14 +1,46 @@
 const { Observable, Subject, ReplaySubject, from, of, range } = rxjs;
-const { map, filter, switchMap ,scan} = rxjs.operators;
+const { map, filter, switchMap ,scan, fromPromise} = rxjs.operators;
+
+const cities = [
+    "Minneapolis",
+    "Chicago"
+];
 
 
-const APIKEY = "049bf60658d7ef8e17926cb7af4eb07c";
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${APIKEY}`)
-    .then(Response => {
-        console.log(Response)
-    })
+const API_KEY = "049bf60658d7ef8e17926cb7af4eb07c";
 
+function getCityWeather(city) {
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+        //.then(res => res.json())
+}
 
+const AJAXStream = Observable.create(
+    observer => {
+        cities.forEach(city=>{
+            console.log("Sending req for ", city);
+            getCityWeather(city).then(
+                HTTPres => {
+                    if (HTTPres.ok) {
+                        HTTPres.json()
+                            .then(JSONres =>{
+                                    console.log("Receiving req for ", city);
+
+                                    observer.next(res)
+
+                                });
+                    }
+                    else {
+                        observer.error(HTTPres)
+                    }
+
+                });
+        })
+    }
+);
+
+const subscription = AJAXStream.subscribe(val=>console.log("Yay: ", val));
+
+// getCityWeather("Minneapolis");
 // range(1, 200)
 //   .pipe(filter(x => x % 2 === 1), map(x => x + x))
 //   .subscribe(x => console.log(x));
